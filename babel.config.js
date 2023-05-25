6a9f563e678ec663af31a5dedd76c560068b07bb
Gabriel Pimenta <gabriel.pimenta@sistemairis.com.br>
2023-05-25 16:58:15 -0300

    🔧 add editorconfig and update configurations

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,26 @@
-module.exports = function(api) {
+module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo'],
+    presets: ["babel-preset-expo"],
+    plugins: [
+      [
+        "module-resolver",
+        {
+          root: ["./src"],
+          extensions: [".js", ".jsx", ".ts", ".tsx", ".json"],
+          alias: {
+            "@components": "./src/components",
+            "@screens": "./src/screens",
+            "@assets": "./src/assets",
+            "@utils": "./src/utils",
+            "@hooks": "./src/hooks",
+            "@config": "./src/config",
+            "@routes": "./src/routes",
+            "@services": "./src/services",
+            "@styles": "./src/styles",
+          },
+        },
+      ],
+    ],
   };
 };
